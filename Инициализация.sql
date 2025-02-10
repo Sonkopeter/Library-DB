9c943e2cd9b857146d2e3b2703a2f198c1b0cad6
Sonkopeter <38082305+Sonkopeter@users.noreply.github.com>
2025-02-10 22:40:01 +0300

    Update Инициализация.sql

--- Инициализация.sql
+++ Инициализация.sql
@@ -2,57 +2,54 @@ DROP TABLE IF EXISTS Authors, Genres, Publishers, Books, Readers, Loans;
 
 DROP TABLE IF EXISTS Authors CASCADE;
 CREATE TABLE Authors (
-    authorID SERIAL PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
+	authorID SERIAL PRIMARY KEY,
+	first_name VARCHAR(100) NOT NULL,
 	patronymic VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL
+    	last_name VARCHAR(100) NOT NULL
 );
 
 DROP TABLE IF EXISTS Genres CASCADE;
 CREATE TABLE Genres (
-    genreID SERIAL PRIMARY KEY,
-    genre_name VARCHAR(100) NOT NULL
+    	genreID SERIAL PRIMARY KEY,
+    	genre_name VARCHAR(100) NOT NULL
 );
 
 DROP TABLE IF EXISTS Publishers CASCADE;
 CREATE TABLE Publishers (
-    publisherID SERIAL PRIMARY KEY,
-    publisher_name VARCHAR(255) NOT NULL
+    	publisherID SERIAL PRIMARY KEY,
+    	publisher_name VARCHAR(255) NOT NULL
 );
 
 DROP TABLE IF EXISTS Books CASCADE;
 CREATE TABLE Books (
-    bookID SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    authorID INT REFERENCES Authors(authorID),
-    genreID INT REFERENCES Genres(genreID),
-    publisherID INT REFERENCES Publishers(publisherID),
-    publication_year INT,
-    loan_count INT DEFAULT 0
+    	bookID SERIAL PRIMARY KEY,
+    	title VARCHAR(255) NOT NULL,
+    	authorID INT REFERENCES Authors(authorID),
+    	genreID INT REFERENCES Genres(genreID),
+    	publisherID INT REFERENCES Publishers(publisherID),
+    	publication_year INT,
+    	loan_count INT DEFAULT 0
 );
 
 DROP TABLE IF EXISTS Readers CASCADE;
 CREATE TABLE Readers (
-    readerID SERIAL PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    email_address VARCHAR(255),
-    phone_number VARCHAR(15)
+    	readerID SERIAL PRIMARY KEY,
+    	first_name VARCHAR(100) NOT NULL,
+    	last_name VARCHAR(100) NOT NULL,
+    	email_address VARCHAR(255),
+    	phone_number VARCHAR(15)
 );
 
 DROP TABLE IF EXISTS Loans CASCADE;
 CREATE TABLE Loans (
-    loanID SERIAL PRIMARY KEY,
-    bookID INT REFERENCES Books(bookID),
-    readerID INT REFERENCES Readers(readerID),
-    loan_date DATE NOT NULL,
-    due_date DATE NOT NULL,
-    return_date DATE,
-    is_overdue BOOLEAN DEFAULT FALSE
+    	loanID SERIAL PRIMARY KEY,
+    	bookID INT REFERENCES Books(bookID),
+    	readerID INT REFERENCES Readers(readerID),
+    	loan_date DATE NOT NULL,
+    	due_date DATE NOT NULL,
+    	return_date DATE,
+    	is_overdue BOOLEAN DEFAULT FALSE
 );
-
-ALTER TABLE Readers 
-RENAME COLUMN address TO email_address;
 
 INSERT INTO Books(bookID, title, authorID, genreID, publisherID, publication_year, loan_count)
 VALUES (1, 'Белая гвардия', 1, 1, 1, 1925, 15),
